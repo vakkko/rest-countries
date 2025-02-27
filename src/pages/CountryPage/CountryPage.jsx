4abffea756e2bdc6fe88f255e7df01f193f6803e
Vako Gabisonia <fgrge@gmail.com>
2025-02-27 22:13:28 +0400

    Style individual country page with proper css.

--- src/pages/CountryPage/CountryPage.jsx
+++ src/pages/CountryPage/CountryPage.jsx
@@ -12,78 +12,84 @@ export default function CountryPage() {
   const borders = countryInfo.borders;
 
   useEffect(() => {
-    if (location.pathname !== "") {
-      document.body.style.backgroundColor = "white";
-    }
-  }, [location]);
-
-  useEffect(() => {
     for (const prop in currencies) {
       setCurrency(currencies[prop].name);
     }
   }, [currencies]);
 
   return (
-    <div>
+    <div className="country-page">
       <div className="back-container">
         <div>
           <Link className="back" to={"/"}>
-            <img src="images/left-arrow.png" alt="left-arrow" />
+            <svg
+              fill="#000000"
+              width="13px"
+              height="13px"
+              viewBox="0 0 476.213 476.213"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <polygon points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5 57.427,253.107 476.213,253.107" />
+            </svg>
             Back
           </Link>
         </div>
       </div>
-      <div>
+      <div className="flag-container">
         <img src={countryInfo.flags.png} alt={countryInfo.flags.alt} />
-      </div>
-      <div>
-        <h1>{countryInfo.name.common}</h1>
-        <ul>
-          <li>
-            <span className="description">Native Name:</span>{" "}
-            <span>{countryInfo.altSpellings[1]}</span>
-          </li>
-          <li>
-            <span className="description">Population:</span>{" "}
-            <span>{countryInfo.population}</span>
-          </li>
-          <li>
-            <span className="description">Region:</span>{" "}
-            <span>{countryInfo.region}</span>
-          </li>
-          <li>
-            <span className="description">Sub Region:</span>{" "}
-            <span>{countryInfo.subregion}</span>
-          </li>
-          <li>
-            <span className="description">Capital:</span>{" "}
-            <span>{countryInfo.capital[0]}</span>
-          </li>
-          <li>
-            <span className="description">Top Level Domain:</span>{" "}
-            <span>{countryInfo.tld[0]}</span>
-          </li>
-          <li>
-            <span className="description">Currencies:</span>{" "}
-            <span>{currency}</span>
-          </li>
-          <li>
-            <span className="description">Languages:</span>{" "}
-            {Object.values(laguages).map((lang, index) => (
-              <span key={index}>{lang} </span>
-            ))}
-          </li>
-        </ul>
-        {borders && (
-          <div>
-            <span>Border Countries:</span>
+        <div>
+          <h2>{countryInfo.name.common}</h2>
+          <ul className="country-info-list">
             <div>
-              {borders.map((country, index) => (
-                <span key={index}>{country}</span>
-              ))}
+              <li>
+                <span className="description">Native Name:</span>{" "}
+                <span>{countryInfo.altSpellings[1]}</span>
+              </li>
+              <li>
+                <span className="description">Population:</span>{" "}
+                <span>{countryInfo.population}</span>
+              </li>
+              <li>
+                <span className="description">Region:</span>{" "}
+                <span>{countryInfo.region}</span>
+              </li>
+              <li>
+                <span className="description">Sub Region:</span>{" "}
+                <span>{countryInfo.subregion}</span>
+              </li>
+              <li>
+                <span className="description">Capital:</span>{" "}
+                <span>{countryInfo.capital[0]}</span>
+              </li>
             </div>
-          </div>
-        )}
+            <div>
+              <li>
+                <span className="description">Top Level Domain:</span>{" "}
+                <span>{countryInfo.tld[0]}</span>
+              </li>
+              <li>
+                <span className="description">Currencies:</span>{" "}
+                <span>{currency}</span>
+              </li>
+              <li>
+                <span className="description">Languages:</span>{" "}
+                {Object.values(laguages).map((lang, index) => (
+                  <span key={index}>{lang} </span>
+                ))}
+              </li>
+            </div>
+          </ul>
+          {borders && (
+            <div className="border-countries">
+              <h2>Border Countries:</h2>
+              <div>
+                {borders.map((country, index) => (
+                  <p key={index}>{country}</p>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
