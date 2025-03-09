f475c9d3df046e185ddffd93f2d5ef443732fe61
Vako Gabisonia <fgrge@gmail.com>
2025-03-09 20:04:04 +0400

    Hide navigation on CountryPagge.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,58 +2,62 @@ import { useContext, useState } from "react";
 import "./navigation.css";
 import ContinentsList from "./continentsList/ContinentsList";
 import { SearchContext } from "../../context/context";
+import { useParams } from "react-router";
 
 export default function Navigation() {
   const [showContinents, setShowContinents] = useState(false);
 
   const search = useContext(SearchContext);
   const { searchValue, setSearchValue } = search;
+  const params = useParams();
 
   const handleChange = (e) => {
     setSearchValue(e.target.value);
   };
 
   return (
-    <>
-      <nav className="country-navigation">
-        <input
-          type="text"
-          placeholder="Search for a country…"
-          value={searchValue}
-          onChange={handleChange}
-        />
-        <button onClick={() => setSearchValue("")} className="clear-btn">
-          clear
-        </button>
-        <div>
-          <div
-            onClick={() => setShowContinents(!showContinents)}
-            className="filter-container"
-          >
-            <button className="filter">Filter by Region</button>
-            <svg
-              fill="#000000"
-              height="5px"
-              width="9px"
-              version="1.1"
-              id="Layer_1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 330 330"
+    !params.country && (
+      <>
+        <nav className="country-navigation">
+          <input
+            type="text"
+            placeholder="Search for a country…"
+            value={searchValue}
+            onChange={handleChange}
+          />
+          <button onClick={() => setSearchValue("")} className="clear-btn">
+            clear
+          </button>
+          <div>
+            <div
+              onClick={() => setShowContinents(!showContinents)}
+              className="filter-container"
             >
-              <path
-                id="XMLID_225_"
-                d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+              <button className="filter">Filter by Region</button>
+              <svg
+                fill="#000000"
+                height="5px"
+                width="9px"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 330 330"
+              >
+                <path
+                  id="XMLID_225_"
+                  d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
               c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
               s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
-              />
-            </svg>
+                />
+              </svg>
+            </div>
+            <ContinentsList
+              setShowContinents={setShowContinents}
+              showContinents={showContinents}
+            />
           </div>
-          <ContinentsList
-            setShowContinents={setShowContinents}
-            showContinents={showContinents}
-          />
-        </div>
-      </nav>
-    </>
+        </nav>
+      </>
+    )
   );
 }
